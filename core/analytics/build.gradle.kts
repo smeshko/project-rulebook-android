69a4a9fe76dffb612fabff4ff0b1cf3e4655c9cd
Ivo Tsonev <a1e6e98@rewe-group.com>
2025-12-15 09:19:52 +0200

    feat(story-1-6-analytics-telemetrydeck): add telemetrydeck sdk dependency
    Updated version catalog to TelemetryDeck SDK 6.3.0 and added implementation
    dependency to core/analytics module with test dependency for unit tests.

--- core/analytics/build.gradle.kts
+++ core/analytics/build.gradle.kts
@@ -7,11 +7,14 @@ android {
 }
 
 dependencies {
-    // TODO: Add TelemetryDeck in Story 1.6
-    // implementation(libs.telemetrydeck)
+    // Analytics
+    implementation(libs.telemetrydeck)
 
     // Koin
     implementation(platform(libs.koin.bom))
     implementation(libs.koin.core)
     implementation(libs.koin.android)
+
+    // Testing
+    testImplementation(libs.junit)
 }
