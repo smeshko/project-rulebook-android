8b886b83313baae11c566c53ed6cb89f65d73adb
Ivo Tsonev <a1e6e98@rewe-group.com>
2026-03-19 11:39:48 +0200

    chore: ship phase deploy artifacts [skip ci]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,8 +7,8 @@ android {
 
     defaultConfig {
         applicationId = "com.rulebook"
-        versionCode = 13
-        versionName = "1.0.12"
+        versionCode = 14
+        versionName = "1.0.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
